91639cd5b9aa8fc266fd426a1693da6ee16aeebe
Rune <r.aasvestad@live.com>
2014-02-06 12:24:24 +0100

    init

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	private const string typeName = "Mohff tutorial game";
+	private const string gameName = "Bathrom";
+	private HostData[] hostList;
+	public GameObject playerPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		if (!Network.isClient && !Network.isServer)
+		{
+			if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+				StartServer();
+			
+			if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
+				RefreshHostList();
+			
+			if (hostList != null)
+			{
+				for (int i = 0; i < hostList.Length; i++)
+				{
+					if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
+						JoinServer(hostList[i]);
+				}
+			}
+		}
+	}
+
+	private void StartServer()
+	{
+		Network.InitializeServer(4, 25001, !Network.HavePublicAddress());
+		MasterServer.RegisterHost(typeName, gameName);
+	}
+
+	void OnServerInitialized()
+	{
+		Debug.Log("Server Initializied");
+		SpawnPlayer();
+	}
+
+	private void RefreshHostList()
+	{
+		MasterServer.RequestHostList(typeName);
+	}
+	
+	void OnMasterServerEvent(MasterServerEvent msEvent)
+	{
+		if (msEvent == MasterServerEvent.HostListReceived)
+			hostList = MasterServer.PollHostList();
+	}
+
+	private void JoinServer(HostData hostData)
+	{
+		Network.Connect(hostData);
+	}
+	
+	void OnConnectedToServer()
+	{
+		Debug.Log("Client Joined");
+		SpawnPlayer();
+	}
+
+	private void SpawnPlayer()
+	{
+		Network.Instantiate(playerPrefab, new Vector3(0f, 0.5f, 0f), Quaternion.identity, 0);
+	}
+	
+
+	void OnDisconnectedFromServer(NetworkDisconnection info) {
+		Debug.Log("This SERVER OR CLIENT has disconnected from a server");
+	}
+	
+	void OnFailedToConnect(NetworkConnectionError error){
+		Debug.Log("Could not connect to server: "+ error);
+	}
+	
+	
+	//Server functions called by Unity
+	void OnPlayerConnected(NetworkPlayer player) {
+		Debug.Log("Player connected from: " + player.ipAddress +":" + player.port);
+	}
+
+	void OnPlayerDisconnected(NetworkPlayer player) {
+		Debug.Log("Player disconnected from: " + player.ipAddress+":" + player.port);
+	}
+	
+	
+	void OnFailedToConnectToMasterServer(NetworkConnectionError info){
+		Debug.Log("Could not connect to master server: "+ info);
+	}
+	
+	void OnNetworkInstantiate (NetworkMessageInfo info) {
+		Debug.Log("New object instantiated by " + info.sender);
+	}
+}
---
+++ Assets/Scripts/ResetPlayer.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+function Update () {
+
+	reset();
+}
+
+function reset(){
+	
+	
+	if(transform.position.y < -4){
+		transform.position.x = 0;
+		transform.position.y = 2;
+		transform.position.z = 0;
+		StartCoroutine(wait());
+	}
+}
+
+function wait(){
+	yield WaitForSeconds(2 * Time.deltaTime);
+}
