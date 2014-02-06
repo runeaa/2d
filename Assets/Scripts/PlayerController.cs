6ccdb674079b83442f85f6d01bb13ae5c59361af
Mohff <vegard@trl188004.ka.hist.no>
2014-02-06 13:56:53 +0100

    Flere scener

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,36 +6,38 @@ public class PlayerController : MonoBehaviour {
 	public float maxSpeed = 10.0f;
 	public float speed = 1.0f;
 	public float jumpSpeed = 2.0f;
-
+	
 	//private Vector2 walk = Vector2.zero;
 	//public Vector2 movementVector = Vector2.zero;
 	private bool grounded = false;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 
-		if (networkView.isMine)
-		{
+		//if (networkView.isMine)
+		//{
 			float move = Input.GetAxis("Horizontal");
 			
 			rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
-		}
+		//}
 	}
 
 	void Update(){
 
-		if(grounded){
-			Debug.Log ("LOL");
-		}
+		//if (networkView.isMine) {
+						if (grounded) {
+								Debug.Log ("LOL");
+						}
 
-		if (Input.GetButtonDown ("Jump")) {
-			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpSpeed);
-		}
+						if (Input.GetButtonDown ("Jump")) {
+								rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, jumpSpeed);
+						}
+				//}
 	}
 
 	void onCollisionEnter(Collision collision){
